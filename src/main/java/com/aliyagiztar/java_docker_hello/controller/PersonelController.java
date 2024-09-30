e527e643ae855d2569eea15822439663c2f1fde7
Ali Yağız Tar <aliyagiz.tar@gmail.com>
2024-09-30 16:10:53 +0300

    Requestmapping fix
    v1 , v2 link fixed

--- src/main/java/com/aliyagiztar/java_docker_hello/controller/PersonelController.java
+++ src/main/java/com/aliyagiztar/java_docker_hello/controller/PersonelController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/api/v1, " ,"/api/v2"})
+@RequestMapping({"/api/v1" ,"/api/v2"})
 public class PersonelController {
 
     // http://localhost:8081/api/v1/hello
