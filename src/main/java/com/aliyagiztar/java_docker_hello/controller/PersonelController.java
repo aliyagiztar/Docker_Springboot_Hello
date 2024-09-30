046cffdc73797bde4734184ee0dfe2835ad5cd4a
Ali Yağız Tar <aliyagiz.tar@gmail.com>
2024-09-30 16:36:39 +0300

    Add PersonelController with basic endpoints for greetings
    - Added '/hello' and '/merhaba' endpoints that return a greeting message.
    - Implemented '/info' endpoint to display current date and time.
    - Configured '/api/v1' and '/api/v2' routes for versioning.

--- src/main/java/com/aliyagiztar/java_docker_hello/controller/PersonelController.java
+++ src/main/java/com/aliyagiztar/java_docker_hello/controller/PersonelController.java
@@ -4,18 +4,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
-@RequestMapping({"/api/v1" ,"/api/v2"})
+@RequestMapping({"/api/v1", "/api/v2"})
 public class PersonelController {
 
     // http://localhost:8081/api/v1/hello
     // http://localhost:8081/api/v1/merhaba
-
     // http://localhost:8081/api/v2/hello
     // http://localhost:8081/api/v2/merhaba
 
     @GetMapping({"/hello", "merhaba"})
-    public String getDockerHello (){
+    public String getDockerHello() {
         return "Selamlar ben Ali Yağız Tar";
+    }
+
+    // http://localhost:8081/api/v1
+    @GetMapping
+    public String devopsHello() {
+        return "DevOps Hello : " + LocalDateTime.now();
+    }
+
+    // http://localhost:8081/api/v1/info
+    // http://localhost:8081/api/v2/info
+    @GetMapping("/info")
+    public String devopsInfo() {
+        return "INFO: " + LocalDateTime.now();
     }
 }
